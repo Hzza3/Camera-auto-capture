46cd6ee0b50e120237cdd33656c4858bbff835d2
Ahmed Hazzaa <ahmed.hazzaa95@gmail.com>
2019-09-26 14:33:34 +0200

    Initial Commit

---
+++ test cam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test cam
+//
+//  Created by Epic Systems on 9/26/19.
+//  Copyright © 2019 epicSystems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
